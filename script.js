4554426d4ebb835cf8c0a59872f1a3327934fe63
sagars0600 <59771375+sagars0600@users.noreply.github.com>
2020-09-18 17:38:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,125 @@
+var elements = 6;
+
+(function() {
+  $('.carousel-logo .item').each(function() {
+    var itemToClone = $(this);
+
+    for (var i = 1; i < elements; i++) {
+      itemToClone = itemToClone.next();
+
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-" + (i))
+        .appendTo($(this));
+    }
+  });
+}());
+
+
+
+$(document).ready(function() {
+  $('.menu-toggle').click(function(){
+    $('nav').toggleClass('active');
+  })
+  
+  $('ul li').click(function(){
+    $(this).siblings().removeClass('active');
+    $(this).toggleClass('active');
+  })
+})
+
+
+
+function lockScroll(){
+  document.body.classList.toggle('lock-scroll');
+}
+
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
+
+$('.brand-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  autoplay:true,
+  responsive:{
+    0:{
+      items:1
+    },
+    600:{
+      items:3
+    },
+    1000:{
+      items:5
+    }
+  }
+})
+
+
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#customers-testimonials').owlCarousel( {
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 30,
+      autoplay: true,
+      dots:true,
+      nav:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+    });
+  });
